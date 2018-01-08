4e9e3d19f9a2b40752b5dd85ba0c40d8b988a3d6
Shmulik <s.chicvashvili@gmail.com>
2018-01-08 17:19:44 +0200

    Fixed the contains definition. and fixed the code accordingly. Now exact_name can be contained in contact_name in vice versa, instead only one way.

--- name_utils/name_utils.py
+++ name_utils/name_utils.py
@@ -7,23 +7,22 @@ def are_names_equal_with_max_distance(name1, name2, distance):
     return Levenshtein.distance(name1, name2) <= distance
 
 
-def _is_either_name_contained(name1, name2):
-    if name1 in name2 or name2 in name1:
-        return True
-
-
 # Found splits need to be greater then 1 in order to return true since we have at least first name and last name that
 # needs to be equal
-def is_contained_name(exact_name, name):
-    if exact_name is None or name is None:
+def _is_first_contained(contained_name, full_name):
+    if full_name is None or contained_name is None:
         return False
     found_splits = 0
-    splitted_name = name.split()
+    splitted_name = contained_name.split()
     for part_name in splitted_name:
-        if _is_either_name_contained(part_name, exact_name):
+        if part_name in full_name:
             found_splits += 1
 
     if found_splits >= MAJORITY_OF_NAME_SPLITS_EQUAL:
         return True
 
     return False
+
+
+def is_contained_name(exact_name, name):
+    return _is_first_contained(exact_name, name) or _is_first_contained(name, exact_name)
